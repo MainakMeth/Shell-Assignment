2be36c8e00ec9987d2220ef75f5449c29b63a969
MainakMeth <91336635+MainakMeth@users.noreply.github.com>
2022-01-08 18:00:04 +0530

    Update marks.sh

--- ASSG 4/marks/marks.sh
+++ ASSG 4/marks/marks.sh
@@ -4,7 +4,7 @@ echo
 cat $file1
 echo 
 
-echo -e ROLL '\t' NAME '\t' MARKS '\t' GRADE >> out1
+echo -e ROLL '\t' NAME '\t' MARKS '\t' GRADE > out1
 
 if [ -f $file1 ]
 then
@@ -13,13 +13,13 @@ then
   do
     set `echo $line`
     case $3 in
-    100|9[0-9]) echo -e '\t' $1 '\t' $2 '\t' $3 '\t'   O >> out1;;
-    8[0-9]) echo -e '\t' $1 '\t' $2 '\t' $3 '\t'  E >> out1;;
-    7[0-9]) echo -e  '\t' $1 '\t' $2 '\t' $3 '\t'  A >> out1;;
-    6[0-9]) echo -e  '\t' $1 '\t' $2 '\t' $3 '\t'  B >> out1;;
-    5[0-9]) echo -e  '\t' $1 '\t' $2 '\t' $3 '\t'  C >> out1;;
-    [0-9]|[1-4][0-9]) echo -e  '\t' $1 '\t' $2 '\t' $3 '\t'  F >> out1;;
-    *) echo -e  '\t' $1 '\t' $2 '\t' $3 '\t' "Invalid" >> out1
+    100|9[0-9]) echo -e $1 '\t' $2 '\t' $3 '\t'   O >> out1;;
+    8[0-9]) echo -e $1 '\t' $2 '\t' $3 '\t'  E >> out1;;
+    7[0-9]) echo -e $1 '\t' $2 '\t' $3 '\t'  A >> out1;;
+    6[0-9]) echo -e $1 '\t' $2 '\t' $3 '\t'  B >> out1;;
+    5[0-9]) echo -e $1 '\t' $2 '\t' $3 '\t'  C >> out1;;
+    [0-9]|[1-4][0-9]) echo -e $1 '\t' $2 '\t' $3 '\t'  F >> out1;;
+    *) echo -e $1 '\t' $2 '\t' $3 '\t' "Invalid" >> out1
     esac
   done
 fi
